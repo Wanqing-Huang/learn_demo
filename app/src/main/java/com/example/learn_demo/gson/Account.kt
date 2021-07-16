9bf5085b4a6494f6d08e57698383e63e49873088
wanqing <wanqing.huang@shopee.com>
2021-07-16 17:05:49 +0800

    gson测试代码

--- app/src/main/java/com/example/learn_demo/gson/Account.kt
+++ app/src/main/java/com/example/learn_demo/gson/Account.kt
@@ -7,5 +7,23 @@ package com.example.learn_demo.gson
 class Account {
     var uid: String? = null
     var userName: String? = null
-    var telNumber: String? = null
+    var telNumber: String = "000000000"
+
+    override fun toString(): String {
+        return "Account(uid=$uid, userName=$userName, telNumber=$telNumber)"
+    }
+}
+
+class Account2(val id: String) {
+    var uid: String? = null
+    var userName: String? = null
+    var telNumber: String = "000000000"
+
+    init {
+        telNumber = "111111111"
+    }
+
+    override fun toString(): String {
+        return "Account2(id='$id', uid=$uid, userName=$userName, telNumber='$telNumber')"
+    }
 }
