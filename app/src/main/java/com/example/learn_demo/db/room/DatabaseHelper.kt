d762eb3956eedc9b003325fc25c5a32cd002f0b2
wanqing <wanqing.huang@shopee.com>
2021-10-22 16:00:22 +0800

    添加room migration demo

--- app/src/main/java/com/example/learn_demo/db/room/DatabaseHelper.kt
+++ app/src/main/java/com/example/learn_demo/db/room/DatabaseHelper.kt
@@ -1,7 +1,5 @@
 package com.example.learn_demo.db.room
 
-import androidx.room.Room
-import com.example.learn_demo.AppUtils
 import com.example.learn_demo.db.room.db.AppDatabase
 
 /**
@@ -9,8 +7,5 @@ import com.example.learn_demo.db.room.db.AppDatabase
  * @date 2021/10/11 6:27 下午
  */
 object DatabaseHelper {
-    val db: AppDatabase = Room.databaseBuilder(
-        AppUtils.context.applicationContext,
-        AppDatabase::class.java, "app_database"
-    ).build()
+    val db: AppDatabase = AppDatabase.build()
 }
--- app/src/main/java/com/example/learn_demo/db/room/db/AppDatabase.kt
+++ app/src/main/java/com/example/learn_demo/db/room/db/AppDatabase.kt
@@ -1,7 +1,11 @@
 package com.example.learn_demo.db.room.db
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.learn_demo.AppUtils
 import com.example.learn_demo.db.room.dao.*
 import com.example.learn_demo.db.room.entity.*
 
@@ -14,12 +18,33 @@ import com.example.learn_demo.db.room.entity.*
 @Database(
     entities = [User::class, Playlist::class, Song::class, PlaylistSongCrossRef::class],
     views = [UserPlayList::class],
-    version = 1
+    version = 2
 )
 abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        fun build(): AppDatabase {
+            return Room.databaseBuilder(
+                AppUtils.context.applicationContext,
+                AppDatabase::class.java, "app_database"
+            ).addMigrations(MIGRATIONS.MIGRATION_1_2)
+                .build()
+        }
+    }
+
+    //======================= 定义dao =====================//
     abstract fun userDao(): UserDao
     abstract fun playListDao(): PlayListDao
     abstract fun songDao(): SongDao
     abstract fun playlistSongCrossRefDao(): PlaylistSongCrossRefDao
     abstract fun userPlayListDao(): UserPlayListDao
+
+    //======================= 定义migration =====================//
+    object MIGRATIONS {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE TABLE IF NOT EXISTS `Song` (`songId` INTEGER NOT NULL, `songName` TEXT NOT NULL, `artist` TEXT NOT NULL, PRIMARY KEY(`songId`))")
+            }
+        }
+    }
 }
